2e7663847766a708ae8494d31774076c934e92b8
MiguelSanLeon <>
2023-11-01 11:45:41 +0000

    Add models and admin panel to booking app

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Booking, UserProfile
 
-# Register your models here.
+
+@admin.register(Booking)
+class BookingComments(admin.ModelAdmin):
+
+    list_filter = ('status', 'created_on', 'booking_date')
+    readonly_fields = ('booking_id',)
+    summernote_fields = ('booking_comments')
+    list_display = (
+        'booking_id', 'user', 'booking_date', 'booking_time', 'guest_num',
+        'status')
+    search_fields = ('user',)
+    actions = ['accept_booking', 'cancel_booking']
+
+    def accept_booking(self, request, queryset):
+        queryset.update(status=1)
+        self.message_user(request, 'Selected bookings have been accepted.')
+
+    def cancel_booking(self, request, queryset):
+        queryset.update(status=2)
+        self.message_user(request, 'Selected bookins have been cancelled.')
+
+    accept_booking.short_description = 'Accept selected bookings'
+    cancel_booking.short_description = 'Cancel selected bookings'
+
+
+@admin.register(UserProfile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'first_name', 'last_name', 'phone_number')
+    search_fields = ('user', 'last_name', 'phone_number')
--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,35 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+STATUS = ((0, 'Booking pending'), (1, 'Booking accepted'),
+          (2, 'Booking cancelled'))
+
+
+class Booking(models.Model):
+    booking_id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='user_bookings')
+    booking_date = models.DateField(auto_now=False)
+    booking_time = models.TimeField(auto_now=False)
+    booking_comments = models.TextField(max_length=300, blank=True, null=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    guest_num = models.PositiveIntegerField()
+    status = models.IntegerField(choices=STATUS, default='0')
+
+    class Meta:
+        ordering = ['-booking_date']
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, primary_key=True, related_name='user_profile')
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=11)
+
+    def __str__(self):
+        return str(self.user)
